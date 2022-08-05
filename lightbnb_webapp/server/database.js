ceb59a7177c1124b977ebf2f8d6066ab2e98d789
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-08-05 21:57:45 +0000

    refactor getAllProperties to include options

--- lightbnb_webapp/server/database.js
+++ lightbnb_webapp/server/database.js
@@ -110,17 +110,74 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function(options, limit = 10) {
 
-  return pool 
-      .query(`SELECT * FROM properties LIMIT $1;`, [limit])
-      .then((result) => {
-        console.log(result.rows);
-        return result.rows;
-        }
-      )
-      .catch((err) => {
-        console.log(err.message);
-        }
-      );
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, AVG(property_reviews.rating) as average_rating 
+  FROM properties
+  JOIN property_reviews ON properties.id = property_reviews.property_id `
+
+  if(options.city){
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if(options.minimum_price_per_night){
+    queryParams.push(options.minimum_price_per_night * 100);
+    if(queryParams.length >= 1){
+      queryString += `AND properties.cost_per_night >= $${queryParams.length} `
+    } else {
+      queryString += `WHERE properties.cost_per_night >= $${queryParams.length} `
+    }
+  }
+
+  if(options.maximum_price_per_night){
+    queryParams.push(options.maximum_price_per_night * 100);
+    if(queryParams.length >= 1){
+      queryString += `AND properties.cost_per_night <= $${queryParams.length} `
+    } else {
+      queryString += `WHERE properties.cost_per_night <= $${queryParams.length} `
+    }
+  }
+
+  if(options.minimum_rating){
+    queryParams.push(options.minimum_rating);
+    if(queryParams.length >= 1){
+      queryString += `AND rating >= $${queryParams.length} `;
+    } else {
+      queryString += `WHERE rating >= $${queryParams.length} `;
+    }
+  }
+
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length}
+  `;
+
+  console.log(options, queryString, queryParams);
+
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {return result.rows});
+
+  // return pool 
+  //     .query(`SELECT properties.*, AVG(property_reviews.rating) as average_rating 
+  //             FROM properties 
+  //             JOIN property_reviews ON properties.id = property_reviews.property_id 
+  //             GROUP BY properties.id
+  //             LIMIT $1;`, [limit])
+  //     .then((result) => {
+  //       console.log(result.rows);
+  //       return result.rows;
+  //       }
+  //     )
+  //     .catch((err) => {
+  //       console.log(err.message);
+  //       }
+  //     );
 };
 exports.getAllProperties = getAllProperties;
 
